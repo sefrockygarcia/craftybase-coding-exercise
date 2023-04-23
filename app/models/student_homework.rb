76576c992e50bc071905bf5e59a2bd542a891f5f
Frocky Garcia <se.frocky@gmail.com>
2023-04-24 07:53:31 +0800

    Add create and index api for student homeworks

--- app/models/student_homework.rb
+++ app/models/student_homework.rb
@@ -2,4 +2,13 @@ class StudentHomework < ApplicationRecord
   belongs_to :student, class_name: User.name
   belongs_to :homework
   has_one_attached :file
+
+  scope :by_grade, ->(grade) {
+    where(grade: grade)
+  }
+
+  scope :by_title, ->(title) {
+    joins(:homework)
+    .where('homeworks.title LIKE ?', "%#{title}%")
+  }
 end
